90fb5f4e52668575f3c9231cedf41791a745d448
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-07-30 20:23:58 +0300

    Adding types/express, and define the right way for handle all requests using middleware

--- Nodejs-ts-sequelize-01/src/server.ts
+++ Nodejs-ts-sequelize-01/src/server.ts
@@ -1,1 +1,17 @@
-console.log("Hello TS World!!!")
+import express, {Request, Response,} from "express"
+
+let app = express()
+
+app.use((req: Request, res: Response) => {
+    return res.status(200).json({
+        "msg": "Server OK"
+    });
+})
+
+app.listen(3000, (error) => {
+    if(error){
+        throw error;
+    }
+
+    console.log("Server listen on port 3000");
+})
