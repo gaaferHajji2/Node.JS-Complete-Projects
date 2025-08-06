4f0d39111fbb8ee9cf268d1cb127cf3cbb530282
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-08-06 19:43:03 +0300

    enable loggin

--- Nodejs-ts-sequelize-01/src/database.config.ts
+++ Nodejs-ts-sequelize-01/src/database.config.ts
@@ -5,7 +5,8 @@ dotenv.config()
 
 var db = new Sequelize("todos", process.env.USERNAME!, process.env.PASSWORD!, {
     dialect: "postgres",
-    port: 5432
+    port: 5432,
+    logging: true,
 })
 
 export default db;
