71a317db9cb73c869d0658e7cc7cc5ee3a8749b5
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-09-27 19:23:19 +0300

    adding optional to completed

--- Nodejs-ts-sequelize-01/src/validation/todo.ts
+++ Nodejs-ts-sequelize-01/src/validation/todo.ts
@@ -6,7 +6,7 @@ class TodoValidator {
         return [
             body('title').notEmpty().withMessage('title is required'),
             body('title').isLength({ min: 2, max: 255 }).withMessage('title length must be between 2 and 255'),
-            body('completed').isBoolean().withMessage('completed must be true or false')
+            body('completed').optional().isBoolean().withMessage('completed must be true or false')
         ]
     }
 }
