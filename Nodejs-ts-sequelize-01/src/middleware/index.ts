4c7ca8271e0efcefe60ef0d2254b5e1ccf4e5c25
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-10-12 01:37:31 +0300

    format doc

--- Nodejs-ts-sequelize-01/src/middleware/index.ts
+++ Nodejs-ts-sequelize-01/src/middleware/index.ts
@@ -1,18 +1,16 @@
 import { NextFunction, Request, Response } from "express";
 import { validationResult } from "express-validator/";
 
-class CheckErrors{
+class CheckErrors {
+  public checkAllErrors(req: Request, res: Response, next: NextFunction) {
+    const errors = validationResult(req);
 
-    public checkAllErrors(req: Request, res: Response, next: NextFunction) {
-        const errors = validationResult(req)
-        
-            if(!errors.isEmpty()) {
-              return res.status(400).json({ errors })
-            }
-        
-            next()
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors });
     }
 
+    next();
+  }
 }
 
-export default new CheckErrors()
+export default new CheckErrors();
