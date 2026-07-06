b68b1141b99e561ae49122e0d78ba1ecf7b70af4
Jafar Loka <gaafer.hajji1995@gmail.com>
2026-07-06 09:34:40 +0300

    Create the stream response function for get numbers and add the service

--- gRPC-NodeJS/server.js
+++ gRPC-NodeJS/server.js
@@ -11,9 +11,26 @@ function sayHello(call, callback) {
   callback(null, reply)
 }
 
+function getNumbers(call) {
+  const count = call.request.numbers
+  let current = 1
+  const interval = setInterval(()=> {
+    if(current > count){
+      clearInterval(interval)
+      call.end()
+      return;
+    }
+    call.write({ order: current, number: current * 100})
+    ++current
+  }, 1000)
+}
+
 function main() {
   const server = new grpc.Server()
-  server.addService(proto.Greeter.service, { SayHello: sayHello })
+  server.addService(proto.Greeter.service, { 
+    SayHello: sayHello,
+    GetNumbers: getNumbers
+  })
   server.bindAsync('0.0.0.0:5050', grpc.ServerCredentials.createInsecure(), (error, port) => {
     if(error){
       console.error("Error in bindAsync method: ", error)
